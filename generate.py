53f84c6e160f3e731b79a4b23fb012550ac65658
amin ben <bencheikh.lehocine.am@gmail.com>
2022-11-14 11:45:25 +0100

    final version

--- generate.py
+++ generate.py
@@ -9,18 +9,19 @@ from models.VQVAE import *
 from models.VanillaVAE import * 
 
 
+
 def load_model():
     input_shape = (28,28,1)
-    checkpoint_path = "./models_checkpoints/betaVAE_latent_8_beta_10.h5"
+    checkpoint_path = "./models_checkpoints/betaVAE_latent_8_beta_20.h5"
 
     model = VanillaVAE(input_shape=input_shape,
                          hidden_dims=[64,128],
                          latent_dim=8,
-                         beta = 10)
+                         beta = 20)
     # Optimizer 
     model.compile(optimizer=tf.keras.optimizers.Adam(), run_eagerly = True)
     X = np.random.normal(size=(1,28,28,1))
-    ok = model(X)
+    _ = model(X)
     model.load_weights(checkpoint_path)
     
     return model 
@@ -30,7 +31,6 @@ if __name__ == '__main__':
 
     # ignore all tensorflow INFO/WARNINGS 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
     parser = argparse.ArgumentParser(description='Variational AutoEncoder .')
     parser.add_argument("--n_samples", type=int, default=1024,
                       help="Number of generated samples.")
---
+++ metrics/inception_score/readme.txt
@@ -0,0 +1,7 @@
+"""
+This folder is taken from this repository 
+We used it only to compute inception score on sampled images 
+requirements to run this code are pytorch , torchvision, opencv2 
+source : https://github.com/sundyCoder/IS_MS_SS 
+
+"""
